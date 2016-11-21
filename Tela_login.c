d86a3c981e23c86e12e2d43d8bb74901862956a1
davibevenuto <davi.bevenuto.cdd@gmail.com>
2016-11-21 07:39:14 -0200

    Alteracao na interface do ADM

--- Tela_login.c
+++ Tela_login.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<stdlib.h>
 #define TAM_NOME 50
 #define TAM_LOGIN 15
 #define TAM_SENHA 15
@@ -5,7 +7,8 @@
 #define QTD_MUSICAS 100
 
 void inicial ();
-
+void Tela_User();
+void Tela_ADM();
 
 typedef struct Usuario {
 	int codigo; // autoincremento
@@ -33,7 +36,7 @@ int musicas[QTD_MUSICAS]; // vetor contendo os códigos das músicas
 } TPlaylist;
 
 
-
+// Programa principal
 int main() {
 
 
@@ -41,40 +44,71 @@ int main() {
 
     do{
     inicial();
-    printf("Digite Uma das opcoes\n");
-    printf("1 - Para programa do ADM\n");
-    printf("2 - Para programa do Usuario\n");
+    printf("Digite Uma das opcoes:\n");
+    printf("1 - Acessar programa do ADM\n");
+    printf("2 - Acessar programa do Usuario\n");
+    printf("0 - Para sair do programa\n");
     scanf("%d",&op);
 
 switch(op){
 
     case 1:
         printf("Menu do Administrador\n");
+        Tela_ADM();
         break;
     case 2:
         printf("Menu do Usuario\n");
+        Tela_User();
         break;
     }
 }while(op!=0);
 
-
-
-
+system("pause");
 	return 0;
 }
+// Fim programa principal
 
-
-// Estrutura do logo do programa
+// Head do programa
 void inicial (){
 
+int i;
 
-printf("\n ___|)_________|___________");
-printf("\n|___/___|______|____");
-printf("\n|__/|___|-.__,-.________");
-printf("\n|_/(|,\\_|/___`-'______");
-printf("\n|_\\_|_/__________");
-printf("\n    | ");
-printf("\n  (_|                      DBS\n");
-
+printf("\t\t\t\t ___|)_________|___________\n");
+printf("\t\t\t\t|___/___|______|____\n");
+printf("\t\t\t\t|__/|___|-.__,-.________\n");
+printf("\t\t\t\t|_/(|,\\_|/___`-'______\n");
+printf("\t\t\t\t|_\\_|_/__________\n");
+printf("\t\t\t\t    | \n");
+printf("\t\t\t\t  (_| \n");
+for(i=0;i<50;i++){
+    printf("--");
+}
+printf("\n");
+printf("\t\t\t\t\tMUSIC PLAYER \n");
+for(i=0;i<50;i++){
+    printf("--");
+}
+printf("\n");
 
 }
+// Fim do head
+// Area de login dos usuarios
+void Tela_User(){
+
+}
+// Fim da tela de login do usuario
+// Area de login do Administrador
+void Tela_ADM(){
+
+    char senha_adm[11]="m@st3r2016",Administrador[TAM_SENHA];
+    int i;
+
+    printf("Por favor digite a senha do Administrador:");
+    for(i=0;i<TAM_SENHA;i++){
+    Administrador[i]=getch();
+    printf("*");
+    }
+    printf("Senha do ADM %s\n",Administrador);
+
+}
+// Fim da tela de login do Administrador
